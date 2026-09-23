267a6f788763002dbdd875cb63c8244c5e50d827
Matheus Felix <matheusfelixf862@gmail.com>
2026-09-23 19:04:26 -0300

    correção jokenpo

--- Desafios/desafio045.py
+++ Desafios/desafio045.py
@@ -1,18 +1,24 @@
 import random
-joken = input('Vamos jogar pedra, papel e tesoura. Escolha sua jogada: ').lower()
+joken = input('Vamos jogar pedra, papel e tesoura. Escolha sua jogada: ').lower().strip()
 lista = ['pedra', 'papel', 'tesoura']
-res = random.choice(lista)
+res = random.choice(lista).strip()
 if res == 'pedra' and joken == 'papel':
     print('Eu escolhi pedra e você papel. Você ganhou!!!')
 elif res == 'pedra' and joken == 'tesoura':
     print('Eu escolhi pedra e você tesoura. Eu ganhei!!!')
+elif res == 'pedra' and joken == 'pedra':
+    print('EMPATE >:(')
 elif res == 'tesoura' and joken == 'papel':
     print('Eu escolhi Tesoura e você Papel. Eu ganhei!!!')
 elif res == 'tesoura' and joken == 'pedra':
     print('Eu escolhi Tesoura e você Pedra. Você ganhou!!!')
+elif res == 'tesoura' and joken == 'tesoura':
+    print('EMPATE >:(')
 elif res == 'papel' and joken == 'pedra':
     print('Eu escolhi Papel e você Pedra. Eu ganhei!!!')
 elif res == 'papel' and joken == 'tesoura':
     print('Eu escolhi Papel e você Tesoura. Você ganhou!!!')
+elif res == 'papel' and joken == 'papel':
+    print('EMPATE >:(')
 else:
     print('Essa resposta não é válida')
